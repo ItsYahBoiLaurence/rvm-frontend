528ebadfd4ec5340448c10edf5af0df3574accdf
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-09-17 11:02:43 +0800

    SignIn Page Done

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { useAuthStore } from "@/stores"
 import type { CredentialType } from "@/types/auth"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 
 const creds: CredentialType = {
     email: 'johnlaurenceburgos@gmail.com',
@@ -10,6 +14,14 @@ const creds: CredentialType = {
 }
 
 export function SignIn() {
+
+    const form = useForm<CredentialType>({
+        defaultValues: {
+            email: "",
+            password: ""
+        }
+    })
+
     const { login, isAuthenticated } = useAuthStore()
     const navigate = useNavigate()
 
@@ -17,16 +29,52 @@ export function SignIn() {
         if (isAuthenticated) navigate('/', { replace: true })
     }, [navigate, isAuthenticated])
 
-    const handleSubmit = async (creds: CredentialType) => {
+    const submit = async (creds: CredentialType) => {
         await login(creds)
+        console.log(creds)
         if (useAuthStore.getState().isAuthenticated) {
             navigate('/')
         }
     }
     return (
-        <>
-            <p>Sign In</p>
-            <Button onClick={() => handleSubmit(creds)}>set</Button>
-        </>
+        <div className="w-full h-screen flex justify-center items-center">
+            <div className="border w-[400px]  p-[20px] flex flex-col gap-2 rounded-lg">
+                <p className="text-center text-xl">Sign In</p>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(submit)} className="flex flex-col gap-5" >
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            rules={{ required: "email is required" }}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} />
+                                    </FormControl>
+                                    {form.formState.errors.email && <FormMessage>{form.formState.errors.email.message}</FormMessage>}
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            rules={{ required: "Password is required" }}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Password</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} type="password" />
+                                    </FormControl>
+                                    {form.formState.errors.password && <FormMessage>{form.formState.errors.password.message}</FormMessage>}
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">Login</Button>
+                    </form>
+                </Form>
+            </div>
+        </div>
     )
 }
